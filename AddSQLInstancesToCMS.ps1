157304f61f42b537d7113a2db8893e68ed1740bc
Jonas Carlsson <jonas.carlsson@atea.se>
2017-03-24 14:17:09 +0100

    Adding functionality to find clustered sql server instances without virtual server computer objects in AD

--- AddSQLInstancesToCMS.ps1
+++ AddSQLInstancesToCMS.ps1
@@ -1,18 +1,62 @@
 ﻿$currentWorkingDirectory = (Get-Item -Path ".\" -Verbose).FullName
 [void][Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO")
 [void][Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Management.RegisteredServers")
-##Using local Invoke-SqlCmd2 until Invoke-SqlCmd2 powershell gallery module is fixed //Jonas 20161122
-. .\Invoke-SqlCmd2.ps1
-<#
+
 if ((Get-Module -ListAvailable -Name "Invoke-Sqlcmd2") -eq $null) {
     Install-Module -Scope CurrentUser -Name "Invoke-Sqlcmd2" -Force
 } else {
     Import-Module -Name "Invoke-Sqlcmd2"
 }
-#>
- 
+
+function Get-ClusteredSQLInstances ([String]$ComputerName){
+    $ClusteredSQLInstances = $null
+    try {
+        $ClusterName = Get-WmiObject -class "MSCluster_Cluster" -namespace "root\mscluster" -computername $ComputerName -Authentication PacketPrivacy -ErrorAction SilentlyContinue | Select-Object Name
+        if ($ClusterName -ne $null) {
+            $ClusteredSQLInstances = Get-WmiObject -class "MSCluster_Resource" -namespace "root\mscluster" -computername $ComputerName -Authentication PacketPrivacy | Where-Object {$_.type -eq "SQL Server"} | Select-Object @{n='Cluster';e={$ClusterName.Name}}, Name, State, @{n='VirtualServerName';e={$_.PrivateProperties.VirtualServerName}}, @{n='InstanceName';e={$_.PrivateProperties.InstanceName}}, @{n='ServerInstance';e={("{0}\{1}" -f $_.PrivateProperties.VirtualServerName,$_.PrivateProperties.InstanceName).TrimEnd('\')}}, @{n='Node';e={$(Get-WmiObject -namespace "root\mscluster" -computerName $ComputerName -Authentication PacketPrivacy -query "ASSOCIATORS OF {MSCluster_Resource.Name='$($_.Name)'} WHERE AssocClass = MSCluster_NodeToActiveResource" | Select-Object -ExpandProperty Name)}}
+    }
+    return $ClusteredSQLInstances
+}catch {
+    return $_
+}
+}
+
+function Update-SQLInstanceRegistration ([String]$SQLInstance, [String]$cmserver, $RegisteredServers, [String]$ErrorLogPath) {
+    $res = $null
+    if ($SQLInstance -ne "" -and $SQLInstance -ne $cmserver) {
+        Write-Output "Checking $SQLInstance"
+        try {
+            $recentError = $null
+            $res = Invoke-Sqlcmd2 -ServerInstance $SQLInstance -ConnectionTimeout 2 -Query "SELECT 1 AS [Success]" -ErrorVariable recentError -ErrorAction SilentlyContinue
+            if ($recentError -ne $null) {
+                $currentDate = Get-Date
+                Out-File -FilePath $ErrorLogPath -Append -InputObject $currentDate
+                Out-File -FilePath $ErrorLogPath -Append -InputObject $SQLInstance
+                Out-File -FilePath $ErrorLogPath -Append -InputObject $recentError
+            }
+            if ($res.Success -eq "1") {
+                if ($RegisteredServers.ServerName -notcontains $SQLInstance) {
+                    $newserver = New-Object Microsoft.SqlServer.Management.RegisteredServers.RegisteredServer($groupstore, $SQLInstance)
+                    $newserver.ServerName = $SQLInstance
+                    $addedDate = Get-Date
+                    $newserver.Description = "Added: $addedDate"
+                    $newserver.Create()
+                    Write-Host "Added Server $SQLInstance" -ForegroundColor Green
+                    $groupstore.Refresh()
+                } else {
+                    Write-Debug "Server $SQLInstance already exists. Skipped"
+                }
+            }
+        }catch {
+            Write-Warning $_
+            Out-File -FilePath $ErrorLogPath -Append -InputObject $_
+        }
+    }
+}
+
 if ((Test-Path -Path "config.json") -eq $false -or $Init -eq $true) {
     Get-Content -Path ".\config.jsontemplate.json" | Out-File -FilePath "config.json"
+    Write-Information "Created configuration file from template."
 }
 $ScriptConfiguration = (Get-Content "config.json") -Join "`n" | ConvertFrom-Json
 if ($ScriptConfiguration.CMS -ne $null) {
@@ -24,8 +68,12 @@ if ($ScriptConfiguration.CMS -ne $null) {
 $server = New-Object Microsoft.SqlServer.Management.Smo.Server $cmserver
 $sqlconnection = $server.ConnectionContext.SqlConnectionObject
 $InventoryFiles = Get-ChildItem -Filter "*.csv" -Path $ScriptConfiguration.InventoryFilePath
-try { $cmstore = new-object Microsoft.SqlServer.Management.RegisteredServers.RegisteredServersStore($sqlconnection)}
-catch { throw "Cannot access Central Management Server" }
+try {
+    $cmstore = new-object Microsoft.SqlServer.Management.RegisteredServers.RegisteredServersStore($sqlconnection)
+}
+catch {
+    throw "Cannot access Central Management Server" 
+}
 $server.ConnectionContext.Disconnect()
 $dbstore = $cmstore.DatabaseEngineServerGroup
 $registeredServers = $dbstore.GetDescendantRegisteredServers()
@@ -59,48 +107,26 @@ foreach ($InventoryFile in $InventoryFiles) {
         $machineName = $foundServer.MachineName
         $SQLInstance = ""
         $instanceName = ""
-        if ($displayName.Contains("$")) {
-            $instanceName = $displayName.Split("$")[1]
-        } elseif (($displayName.StartsWith("SQL Server (")) -and ($displayName.EndsWith(")"))) {
-            $instanceName = $displayName.Replace("SQL Server (", "")
-            $instanceName = $instanceName.Replace(")", "")
-        }
-        if (($instanceName -ne "") -and ($instanceName -ne "MSSQLSERVER")) {
-            $SQLInstance = "$machineName\$instanceName"
-        } else {
-            $SQLInstance = "$machineName"
-        }
-        if ($SQInstance -eq $cmserver) {
-            continue
-        }
-        $res = $null
-        if ($SQLInstance -ne "" -and $SQLInstance -ne $cmserver) {
-            try {
-                $recentError = $null
-                $res = Invoke-Sqlcmd2 -ServerInstance $SQLInstance -ConnectionTimeout 2 -Query "SELECT 1 AS [Success]" -ErrorVariable recentError -ErrorAction SilentlyContinue
-                if ($recentError -ne $null) {
-                    $currentDate = Get-Date
-                    Out-File -FilePath $ErrorLogPath -Append -InputObject $currentDate
-                    Out-File -FilePath $ErrorLogPath -Append -InputObject $SQLInstance
-                    Out-File -FilePath $ErrorLogPath -Append -InputObject $recentError
-                }
-                if ($res.Success -eq "1") {
-                    if ($RegisteredServers.ServerName -notcontains $SQLInstance) {
-                        $newserver = New-Object Microsoft.SqlServer.Management.RegisteredServers.RegisteredServer($groupstore, $SQLInstance)
-                        $newserver.ServerName = $SQLInstance
-                        $addedDate = Get-Date
-                        $newserver.Description = "Added: $addedDate"
-                        $newserver.Create()
-                        Write-Host "Added Server $SQLInstance" -ForegroundColor Green
-                        $groupstore.Refresh()
-                    } else {
-                    Write-Debug "Server $SQLInstance already exists. Skipped"
-                    }
-                }
-            }catch {
-            Write-Warning $_
-            Out-File -FilePath $ErrorLogPath -Append -InputObject $_
+        $ClusteredInstances = $null
+        
+        $ClusteredInstances = @(Get-ClusteredSQLInstances -ComputerName $machineName)
+        if ($ClusteredInstances -ne $null) {
+            foreach ($ClusteredInstance in $ClusteredInstances) {
+                Update-SQLInstanceRegistration -SQLInstance $ClusteredInstance.ServerInstance -cmserver $cmserver -RegisteredServers $RegisteredServers -ErrorLogPath $ErrorLogPath 
             }
+        } else {
+            if ($displayName.Contains("$")) {
+                $instanceName = $displayName.Split("$")[1]
+            } elseif (($displayName.StartsWith("SQL Server (")) -and ($displayName.EndsWith(")"))) {
+                $instanceName = $displayName.Replace("SQL Server (", "")
+                $instanceName = $instanceName.Replace(")", "")
+            }
+            if (($instanceName -ne "") -and ($instanceName -ne "MSSQLSERVER")) {
+                $SQLInstance = "$machineName\$instanceName"
+            } else {
+                $SQLInstance = "$machineName"
+            }
+            Update-SQLInstanceRegistration -SQLInstance $SQLInstance -cmserver $cmserver -RegisteredServers $RegisteredServers -ErrorLogPath $ErrorLogPath
         }
-    }
+   }
 }
